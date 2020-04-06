3a1e898037885824ea6850e4de66f7d0abe29e17
Rajat Kotian <rajat.kotian@gmail.com>
2020-04-07 01:10:14 +0530

    Header , Header Links and Header Image Changes

--- blog_website/src/Layout/Header/Header.js
+++ blog_website/src/Layout/Header/Header.js
@@ -1,24 +1,36 @@
 import React from 'react'
-import {Container ,Row,Col } from 'react-bootstrap'
+import { Container, FormControl, NavDropdown, Navbar, Nav, Form, Button, Col, Row } from 'react-bootstrap'
+import Sunset from './../../_.jpeg'
 
-export default class Header extends React.Component{
-    constructor (props){
+export default class Header extends React.Component {
+    constructor(props) {
         super(props)
         this.state = {
-            
+
         }
     }
-    render (){
-        return(
+    render() {
+        return (
             <div >
-
-                <Container fluid style = {{backgroundColor:"black"}}>
-                    <Row>
-                        <Col>1 of 1</Col>
-                    </Row>
+                <Container fluid className="navbar navbar-expand-lg flex-row" style={{ backgroundColor: "#522d5b", height: "10em" }}>
+                    <Navbar.Brand href="#home" style={{ padding: "10px 15px" }}>My Random Thoughts</Navbar.Brand>
+                    <Nav className="ml-auto">
+                        <Col>
+                            <Row>
+                                <Nav.Link href="#home">Home</Nav.Link>
+                                <Nav.Link href="#features">Features</Nav.Link>
+                                <Nav.Link href="#pricing">Pricing</Nav.Link>
+                            </Row>
+                        </Col>
+                    </Nav>
+                </Container>
+                <Container fluid style={{ padding: "0", margin: "0" }}>
+                    <div className="img-card" >
+                        <img src={Sunset} alt="Default_Image" style={{ height: "100%", width: "100%" }} />
+                    </div>
                 </Container>
             </div>
-            
+
         )
     }
 
